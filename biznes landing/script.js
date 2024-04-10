c23d09a9c134ac994139246b90900d1be4f967f5
Josue Zavala <josue.zavala.projects@gmail.com>
2024-04-09 21:01:10 -0600

    Correcting tab pagination

--- biznes landing/script.js
+++ biznes landing/script.js
@@ -1,85 +1,83 @@
 ///////////////////////////////////////////////////////////
 // Make mobile navigation work
 const btnNavEl = document.querySelector(".btn-mobile-nav");
-const headerEl = document.querySelector(
-    ".header");
+const headerEl = document.querySelector(".header");
 
-btnNavEl.addEventListener("click", function() {
-    headerEl.classList.toggle("nav-open");
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
 });
 
 // Smooth scrolling animation
 
 const allLinks = document.querySelectorAll("a:link");
 
-allLinks.forEach(function(link) {
-    link.addEventListener("click", function(e) {
-        e.preventDefault();
-        const href = link.getAttribute("href");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
 
-        // scroll back to top
-        if (href === "#")
-            window.scrollTo({
-                top: 0,
-                behaviour: "smooth",
-            })
-            // scroll to other links
-        if (href !== "#" && href.startsWith("#")) {
-            const sectionEl = document.querySelector(href);
-            sectionEl.scrollIntoView({
-                behaviour: "smooth"
-            });
-        }
+    // scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behaviour: "smooth",
+      });
+    // scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({
+        behaviour: "smooth",
+      });
+    }
 
-        // Close mobile navigatoin
-        if (link.classList.contains("main-nav-link"))
-            headerEl.classList.toggle("nav-open");
-    });
+    // Close mobile navigatoin
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
 });
-
 
 //  Sticky navigation
 
 const sectoinHeroEl = document.querySelector(".section-hero");
 
-const obs = new IntersectionObserver(function(entries) {
+const obs = new IntersectionObserver(
+  function (entries) {
     const ent = entries[0];
     console.log(ent);
 
     if (ent.isIntersecting == false) {
-        document.body.classList.add("sticky");
+      document.body.classList.add("sticky");
     }
     if (ent.isIntersecting === true) {
-        document.body.classList.remove("sticky");
+      document.body.classList.remove("sticky");
     }
-}, {
+  },
+  {
     // In the viewport
     root: null,
     threshold: 0,
     rootMargin: "-80px",
-});
+  }
+);
 
 obs.observe(sectoinHeroEl);
 
-
-
-
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-    var flex = document.createElement("div");
-    flex.style.display = "flex";
-    flex.style.flexDirection = "column";
-    flex.style.rowGap = "1px";
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
 
-    flex.appendChild(document.createElement("div"));
-    flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
 
-    document.body.appendChild(flex);
-    var isSupported = flex.scrollHeight === 1;
-    flex.parentNode.removeChild(flex);
-    console.log(isSupported);
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
 
-    if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
 checkFlexGap();
 
